16fd0d48c481b79cbea5e19598f2b82a32125037
Alina Rashitova <alinarashitova02@yandex.ru>
2023-01-12 22:13:23 +0400

    fix after tests

--- app.js
+++ app.js
@@ -25,16 +25,16 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
 });
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/', usersRouter);
-app.use('/', cardsRouter);
 app.use((req, res, next) => {
   req.user = {
-    _id: '63bedb6d27ad78938bf82db5',
+    _id: '63c039e916328352a785d0b5',
   };
 
   next();
 });
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/', usersRouter);
+app.use('/', cardsRouter);
 
 app.listen(PORT);
