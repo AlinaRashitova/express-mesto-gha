0090dec925ba60a1d390e94a2f44debc1cdd3d51
Alina Rashitova <alinarashitova02@yandex.ru>
2023-01-29 22:49:00 +0400

    14th project

--- app.js
+++ app.js
@@ -1,34 +1,68 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 
-const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const { NOT_FOUND_ERROR_CODE } = require('./utils/constatnts');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+const { errors, celebrate, Joi } = require('celebrate');
+
+const app = express();
+
+const { createUser, login } = require('./controllers/users');
+const handleErrors = require('./middlewares/errors');
+const NotFoundError = require('./errors/not-found-error');
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
 
-// const path = require('path');
-
 const { PORT = 3000 } = process.env;
+
+const limiter = rateLimit({
+  windowsMs: 15 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
 
 mongoose.set('strictQuery', false);
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
 });
 
-app.use((req, res, next) => {
-  req.user = {
-    _id: '63c039e916328352a785d0b5',
-  };
+app.use(cookieParser());
 
-  next();
-});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/', usersRouter);
-app.use('/', cardsRouter);
-app.use((req, res) => {
-  res.status(NOT_FOUND_ERROR_CODE).send({ message: 'Ошибка 404: несуществующая страница' });
+
+app.use(helmet());
+
+app.use(limiter);
+
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+}), login);
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().pattern(/[-a-zA-Z0-9@:%_+.~#?&/=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_+.~#?&/=]*)?/i),
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+}), createUser);
+
+app.use('/users', usersRouter);
+app.use('/cards', cardsRouter);
+
+app.use((req, res, next) => {
+  next(new NotFoundError('Страница не найдена'));
 });
+
+app.use(errors());
+
+app.use(handleErrors);
 
 app.listen(PORT);
